96921381479fcda7a293d983904fb2643651b010
metastableB <donkdennis@gmail.com>
2015-06-26 11:47:07 +0530

    Definitions for overloaded transfer functions

--- src/transferunit.h
+++ src/transferunit.h
@@ -45,8 +45,15 @@ class TransferUnit {
 	std::string dekatronArrayToString(Dekatron* arr, int size);
 public:
 	// TODO: Transfer unit does not check if stores are from the same set
+	// Transfer from store to store
 	void transfer(DekatronStore* sStore, DekatronStore* rStore, int shiftAmount = 0);
 	void transferComplement(DekatronStore* sStore, DekatronStore* rStore, int shiftAmount = 0);
+	// Transfer from accum to store
+	void transfer(Accumulator* accum, DekatronStore* rStore, int shiftAmount = 0 );
+	void transferComplement(Accumulator* accum, DekatronStore* rStore, int shiftAmount = 0 );
+	// Transfer from store to accum
+	void tranfer(DekatronStore* sStore, Accumulator* accum, int shiftAmount = 0);
+	void tranferComplement(DekatronStore* sStore, Accumulator* accum, int shiftAmount = 0);
 };
 #endif /* TRANSFERUNIT_H */
 
