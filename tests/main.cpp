05d091a5c695320e63027d9078fef10fed04ba3c
metastableB <donkdennis@gmail.com>
2015-06-27 16:45:29 +0530

    Test Report error count : driver(main)

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,26 +7,33 @@
  *
  */
 #include <iostream>
+#include <string>
 #include "testdekatron.h"
 #include "testdekatronstore.h"
 #include "testtransferunit.h"
 #include "testshiftcircuit.h"
 #include "testaccumulator.h"
 int main(int argc , char *argv[]) {
+	std::string report;
 #ifdef TEST_DEKATRON
 	TestDekatron dekatron;
+	report += "\ndekatron.cpp       Failures = " + std::to_string(dekatron.failedTests);
 #endif /*TEST_DEKATRON*/
 #ifdef TEST_DEKATRONSTORE
 	TestDekatronStore store;
+	report += "\ndekatronstore.cpp  Failures = " + std::to_string(store.failedTests);
 #endif /*TEST_DEKATRONSTORE*/
 #ifdef TEST_TRANSFERUNIT
 	TestTransferUnit transferUnit;
+	report += "\ntransferunit.cpp   Failures = " + std::to_string(transferUnit.failedTests);
 #endif /*TEST_TRANSFERUNIT*/
 #ifdef TEST_SHIFTCIRCUIT
-	TestShiftCircuit testShiftCircuit;
+	TestShiftCircuit shiftCircuit;
+	report += "\nshiftcircuit.cpp   Failures = " + std::to_string(shiftCircuit.failedTests);
 #endif /*TEST_SHIFTCIRCUIT*/
 #ifdef TEST_ACCUMULATOR
 	TestAccumulator testAccumulator;
+	report += "\naccumulator.cpp    Failures = " + std::to_string(testAccumulator.failedTests);
 #endif /*TEST_ACCUMULATOR*/
-	std::cout << "\n:::END TEST:::\n";
+	std::cout << "\n:::END TEST:::\n" << report <<"\n\n";
 }
