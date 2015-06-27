39e39312bdc71d69850340eab75f6e8c2dc0e2f2
metastableB <donkdennis@gmail.com>
2015-06-27 22:50:11 +0530

    multiplication with positive multipplier

--- src/alu.cpp
+++ src/alu.cpp
@@ -20,5 +20,31 @@ bool ALU::subtract(DekatronStore* sStore, DekatronStore* rStore){
 	return false;
 }
 
+bool ALU::multiply(DekatronStore* sStore, DekatronStore* rStore, Accumulator* accum){
+	/*
+	 * Algorithm :
+	 *	If multiplier is positive :
+	 *	For each digit of the multiplier taken from left to right (current digit being i in [1,8])
+	 *		while (digit > 0)
+	 *			Add the multiplicand to the accm shifted i times
+	 *			Decrement the digit
+	 *
+	 */
+	// TODO : Check for overflow
+	int currentDigit = 0;
+	if(rStore->getStateIn(0) == DekatronState::ZERO) {
+		for(int i = 1; i < 9; i++){
+			currentDigit = int(rStore->getStateIn(i));
+			while( currentDigit > 0){
+				transferUnit.transfer(sStore,accum,i-1);
+				currentDigit--;
+				rStore->setStoreValueIn(i,currentDigit);
+			}
+		}
+	}
+	return false;
+
+}
+
 
 
--- src/alu.h
+++ src/alu.h
@@ -11,19 +11,19 @@
 #define ALU_H
 
 #include "accumulator.h"
+#include "dekatron.h"
 #include "dekatronstore.h"
 #include "transferunit.h"
 
 class ALU {
 	// TODO : [Reminder] since we have more than one accum, we use a pointer here
-	Accumulator* accumulator;
 	TransferUnit transferUnit;
 public :
 	// TODO : the return value to be used for overflow checks
 	bool add(DekatronStore* sStore, DekatronStore* rStore);
 	bool subtract(DekatronStore* sStore, DekatronStore* rStore);
-	bool multiply(DekatronStore* sStore, DekatronStore* rStore);
-	bool divide(DekatronStore* sStore, DekatronStore* rStore);
+	bool multiply(DekatronStore* sStore, DekatronStore* rStore, Accumulator* accum);
+	bool divide(DekatronStore* sStore, DekatronStore* rStore, Accumulator* accum);
 
 };
 
