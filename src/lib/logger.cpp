1ca0c9d1e969d0fcdaf786a5adb7763b430309f5
metastableB <donkdennis@gmail.com>
2015-07-18 20:23:36 +0530

    Using logger in accum_tests

--- src/lib/logger.cpp
+++ src/lib/logger.cpp
@@ -12,16 +12,16 @@
 void Logger::log(LogLevel level, std::string origin,std::string msg) {
 	switch(level) {
 	case LogLevel::DEBUG :
-		std::cerr << "[DEBUG] : " << origin << " : "<< msg;
+		std::cerr << "[DEBUG] " << origin << ": "<< msg;
 		break;
 	case LogLevel::INFO :
-		std::cerr << "[INFO] : " << origin << " : "<< msg;
+		std::cerr << "[INFO] " << origin << ": "<< msg;
 		break;
 	case LogLevel::WARNING :
-		std::cerr << "[WARNING] : " << origin <<" : "<< msg;
+		std::cerr << "[WARNING] " << origin <<": "<< msg;
 		break;
 	case LogLevel::ERROR :
-		std::cerr << "[ERROR] : " << origin << " : "<< msg;
+		std::cerr << "[ERROR] " << origin << ": "<< msg;
 		break;
 	}
 }
