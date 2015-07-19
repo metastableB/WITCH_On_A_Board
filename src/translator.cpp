3e23036381e00912171d41bf496cab0a9e190e9c
metastableB <donkdennis@gmail.com>
2015-07-19 23:32:46 +0530

    translator store to dekatronStore defined

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,5 +1,5 @@
 /*
-] * translator.cpp
+ * translator.cpp
  *
  * @author Don Dennis (metastableB)
  * donkdennis [at] gmail [dot] com
@@ -10,10 +10,39 @@
 #include "translator.h"
 
 bool Translator::store(std::string input, DekatronStore* store) {
-	int sign = input.at(0) - '0';
-	if(sign != 9 || sign != 0)
-		// TODO : logoutput
+	// input format is +ve[8 digits], -ve[8 digits], *[5 digits]
+	char sign = input.at(0);
+	if(sign != '+' || sign != '-' || sign != '*') {
+		logObject.log( LogLevel::ERROR,"translator.cpp", "Received object with undefined sign.\n");
 		return false;
+	}else if (input.length() != 9 || input.length() != 5){
+		logObject.log(LogLevel::ERROR, "translator.cpp","Received undefined input length.\n");
+		return false;
+	}
+	if(sign == '+' && input.length() == 9) {
+		store->setStoreValueIn(0,0);
+		for(int i = 1; i < 9; i++)
+			store->setStoreValueIn(i, input.at(i) - '0');
+		return true;
+	} else if ( sign == '-' && input.length() == 9) {
+		store->setStoreValueIn(0,9);
+		for(int i = 1; i < 9; i++)
+			store->setStoreValueIn(i, 9 -(input.at(i) - '0'));
+		return true;
+	} else if (sign == '*' && input.length() == 5) {
+		store->setStoreValueIn(0,0);
+		for(int i = 1; i < 6; i++)
+			store->setStoreValueIn(i, input.at(i) - '0');
+		for(int i = 6; i <= 9; i++)
+			store->setStoreValueIn(i, 0);
+		return true;
+	} else {
+		logObject.log(LogLevel::ERROR, "translator.cpp","Received undefined sign -value pair.");
+		return false;
+	}
+	// Control should not reach here
+	logObject.log(LogLevel::ERROR, "translator.cpp","Undefined behavior.");
+	return false;
 }
 
 
--- src/translator.h
+++ src/translator.h
@@ -13,10 +13,12 @@
 #include "dekatron.h"
 #include "dekatronstore.h"
 #include "accumulator.h"
+#include "lib/logger.h"
 // TODO : This object currently acts as a place holder till we are ready with
 //	  the details of how the translator actually works
 
 class Translator {
+	Logger logObject;
 public:
 	// Store string_input of length 5/8 at sore
 	// return if error!
