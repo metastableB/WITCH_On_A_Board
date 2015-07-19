3d4926a625bfbbdbf9221963ffd6bade8de7818c
metastableB <donkdennis@gmail.com>
2015-07-19 23:44:29 +0530

    definitions for store size

---
+++ src/lib/definitions.h
@@ -0,0 +1,25 @@
+/*
+ * definitions.h
+ *
+ * Contains global macro definitions
+ *
+ * @author Don Dennis (metastableB)
+ * donkdennis [at] gmail [dot] com
+ * 19-Jul-2015
+ * 
+ */
+
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+/* DekatronStore Array[row][col] form to index the dekatron stores
+ * row in [1,9] and column in [0,9]
+ */
+#define NO_OF_STORE_ROW 10
+#define NO_OF_STORE_COL	10
+
+#endif /* DEFINITIONS_H */
+
+
+
+
