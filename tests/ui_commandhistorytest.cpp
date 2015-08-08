bfff2e843879f2629cb1685791fb21b74fa768e0
metastableB <donkdennis@gmail.com>
2015-08-09 03:11:25 +0530

    Incomplete : ui_commandhistorytest.cpp

---
+++ tests/ui_commandhistorytest.cpp
@@ -0,0 +1,38 @@
+/*
+ * ui_commandhistorytest.cpp
+ *
+ * @author Don Dennis (metastableB)
+ * donkdennis [at] gmail [dot] com
+ * 29-Jul-2015
+ * 
+ */
+
+#include "../src/cli/commandhistory.h"
+#include "../src/cli/commandhistory.h"
+
+#include <iostream>
+#include <cstdio>
+
+/*
+ * THIS FILE IS INCOMPLETE
+ */
+using namespace std;
+int main(){
+	char c;
+	string line;
+	int charCount = 0;
+	while(true){
+		cout << "WITCH-e >> ";
+		c = 'a'
+		while(c!= '\0'){
+			c = getchar();
+			ungetc(c,stdout);
+			if(c == "");
+		}
+	}
+	return 0;
+}
+
+
+
+
