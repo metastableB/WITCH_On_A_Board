7f0235284f517bed066784f4981747d934ba8a07
metastableB <donkdennis@gmail.com>
2015-08-08 22:12:19 +0530

    Global Validater

---
+++ src/cli/ordervalidater.cpp
@@ -0,0 +1,17 @@
+/*
+ * ordervalidater.cpp
+ *
+ *  Created on: 08-Aug-2015
+ *      Author: metastableB
+ */
+
+#include "ordervalidater.h"
+
+ValidaterStatus OrderValidater::validateOrder(std::string s){
+
+	return ValidaterStatus::INVALID_ORDER;
+}
+
+
+
+
